459520c571579de4d2d3cbc44143b371c4c1ab19
root <eric.lee.santos@gmail.com>
2019-11-27 20:50:31 -0300

    oi gente

--- backend/api.js
+++ backend/api.js
@@ -3,8 +3,11 @@ const machine = require('./functions');
 exports.build = function(server){
 
   server
-  	.get('/techtrack/funtion/cadastro', machine.get_machine)
-    .post('/techtrack/funtion/cadastro', machine.add_machine)
-    .get('/techtrack/funtion/dados', machine.get_data)
-    .post('/techtrack/funtion/dados', machine.add_data)
+  	.get('/techtrack/funtion/cadastro', functions.get_machine)
+    .post('/techtrack/funtion/cadastro', functions.add_machine)
+    .get('/techtrack/funtion/dados', functions.get_data)
+    .post('/techtrack/funtion/dados', functions.add_data)
+    .post('/techtrack/funtion/dados', functions.delete_machine)
+    .post('/techtrack/funtion/dados', functions.update_machine)
 }
+get_data, get_machine, add_machine, add_data, delete_machine, update_machine
