95d220c23d98cf77ad3dcd87d324daf6c6b6c587
root <eric.lee.santos@gmail.com>
2019-11-27 18:49:31 -0300

    alterando configs

--- backend/api.js
+++ backend/api.js
@@ -3,10 +3,8 @@ const machine = require('./machine');
 exports.build = function(server){
 
   server
-  	.get('/desafio3/info/cadastro', machine.get_machine)
-    .post('/desafio3/info/cadastro', machine.add_machine)
-    .get('/desafio3/info/dados', machine.get_data)
-    .post('/desafio3/info/dados', machine.add_data)
-
-    .get('/desafio3/info/dados/maquina/:id', machine.get_data_from_machine)
+  	.get('/techtrack/funtion/cadastro', machine.get_machine)
+    .post('/techtrack/funtion/cadastro', machine.add_machine)
+    .get('/techtrack/funtion/dados', machine.get_data)
+    .post('/techtrack/funtion/dados', machine.add_data)
 }
