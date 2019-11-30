36b2c6083e8c4f614e48926a5cd444c251ddb51d
System Administrator <root@MacBook-Pro-de-Eric.local>
2019-11-30 18:34:52 -0300

    finalizando maquinas.js

--- frontend/js/maquinas.js
+++ frontend/js/maquinas.js
@@ -1,3 +1,17 @@
+function success(){
+	var alert  = '<div class="alert alert-success alert-dismissible fade show" role="alert">O cadastro foi realizado com sucesso!'
+	    alert += '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span> </button></div>';
+
+	$('.container').prepend(alert);
+}
+
+function fail(){
+	var alert  = '<div class="alert alert-warning alert-dismissible fade show" role="alert">O cadastro nao pode ser realizado, verifique as opcoes e preencha novamente!'
+	    alert += '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span> </button></div>';
+
+	$('.container').prepend(alert);
+}
+
 function updateList(dados){
 	$("#listTable tbody").empty()
 	var table_html = ''
@@ -13,11 +27,8 @@ function updateList(dados){
 	$("#listTable tbody").html(table_html);
 }
 
-
-
-
 function submitAdd(){
-	$("#addForm").on('click',function(event){
+	$("#AddForm").on('submit',function(event){
 		event.preventDefault();
 
 		var addInfo = $('#AddForm').serializeArray();
@@ -27,10 +38,10 @@ function submitAdd(){
 			addJson[input_data.name]=input_data.value;
 		})
 
-		var request =$.ajax({
+		var request = $.ajax({
 			method: "POST",
 			url: "/techtrack/func/cadastro/add",
-			data: JSON.stringy(addJson),
+			data: JSON.stringify(addJson),
 			contentType: "application/json"
 			dataType:"json",
 		})
@@ -43,14 +54,76 @@ function submitAdd(){
 		request.done(function(resp){
 			console.log('DONE: ',resp)
 			success();
+			updateList();
+		})
+	})
+}
+
+function submitDel(){
+	$("#DelForm").on('submit',function(event){
+		event.preventDefault();
+
+		var delInfo = $('#DelForm').serializeArray();
+		var delJson = {};
+
+		modInfo.forEach(function(input_data){
+			delJson[input_data.name]=input_data.value;
+		})
+
+		var request = $.ajax({
+			method: "POST",
+			url: "/techtrack/func/cadastro/del",
+			data: JSON.stringify(delJson),
+			contentType: "application/json"
+			dataType:"json",
+		})
+
+		request.fail(function(failed){
+			console.log('FAIL: ',failed)
+			fail();
+		})
+
+		request.done(function(resp){
+			console.log('DONE: ',resp)
+			success();
+			updateList();
 		})
 
 	})
 }
 
+function submitMod(){
+	$("#ModForm").on('submit',function(event){
+		event.preventDefault();
 
+		var modInfo = $('#ModForm').serializeArray();
+		var modJson = {};
 
+		modInfo.forEach(function(input_data){
+			modJson[input_data.name]=input_data.value;
+		})
 
+		var request = $.ajax({
+			method: "POST",
+			url: "/techtrack/func/cadastro/update",
+			data: JSON.stringify(modJson),
+			contentType: "application/json"
+			dataType:"json",
+		})
+
+		request.fail(function(failed){
+			console.log('FAIL: ',failed)
+			fail();
+		})
+
+		request.done(function(resp){
+			console.log('DONE: ',resp)
+			success();
+			updateList();
+		})
+
+	})
+}
 
 $(document).ready(function()
 {
@@ -62,4 +135,7 @@ $(document).ready(function()
 	}).fail(function(){
 		alert("error");
   })
+	submitAdd();
+	submitDel();
+	submitMod();
 })
