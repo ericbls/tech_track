036648834f2dadffc98cc22edc7d741b76d13495
ericbls <eric.lee.santos@gmail.com>
2019-12-01 17:14:09 -0300

    tentando plotar gráfico

--- frontend/js/stats.js
+++ frontend/js/stats.js
@@ -20,10 +20,44 @@ function updateList(){
 	})
 }
 
+function lineChart(){
+	var ctx = document.getElementsById('lineChart').getContext('2d');
+	var varChart = new Chart(ctx,{
+		type: 'line',
+		data: {
+			labels:["1","2","3","4","5","6","7"],
+			datasets:[
+				{
+					label:"Máquina1",
+					data:[10,20,30,40,50,60,70],
+					pointStyle:'cross'
+				},
+				{
+					label:"Máquina2",
+					data:[70,60,50,40,30,20,10]
+				}
+			]
+		};,
+		options: {
+			title:{
+				display:true,
+				text:'Tempo de uso por máquina (em horas)'
+			},
+			scales:{
+				yAxes:[{
+					ticks:{
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
+}
 
 
 $(document).ready(function(){
+	lineChart();
 	setInterval(function(){
 		updateList()
-	},5000);	
+	},5000);
 })
