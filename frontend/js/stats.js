1db1714c0774b10f0cbfc2eec2330b35dea39f51
ericbls <eric.lee.santos@gmail.com>
2019-12-01 12:46:53 -0300

    adicionando PEN, mudando stats.js e stats.html

--- frontend/js/stats.js
+++ frontend/js/stats.js
@@ -1,33 +1,28 @@
-function updateList(dados){
-	$("#listTable tbody").empty()
-	var table_html = ''
+function updateList(){
+	$.ajax({
+		method: "GET",
+		url: "/techtrack/func/dados",
+	}).done(function(resp){
+		$("#listTable tbody").empty()
+		var table_html = ''
 
-	dados.forEach(function(item, index){
-		table_html += "<tr>"
-		table_html += "<th>" + index + "</th>"
-		table_html += "<th>" + item.id_maquina + "</th>"
-		table_html += "<th>" + item.ip + "</th>"
-		table_html += "<th>" + item.pmc_alm + "</th>"
-		table_html += "<th>" + item.alm_stat + "</th>"
-		table_html += "<th>" + item.emg_stat + "</th>"
-		table_html += "<th>" + item.run_stat + "</th>"
-		table_html += "<th>" + item.motion_stat + "</th>"
-		table_html += "<th>" + item.time + "</th>"
-		table_html += "<th>" + item.date + "</th>"
-		table_html += "</tr>"
+		resp.forEach(function(item, index){
+			table_html += "<tr>"
+			table_html += "<th>" + item.id_maq + "</th>"
+			table_html += "<th>" + item.ip + "</th>"
+			table_html += "<th>" + item.run + "</th>"
+			table_html += "<th>" + item.data + "</th>"
+			table_html += "</tr>"
+		})
+		$("#listTable tbody").html(table_html);
+	}).fail(function(){
+		alert("error");
 	})
-	$("#listTable tbody").html(table_html);
 }
 
 
+
 $(document).ready(function(){
-	$.ajax({
-		method: "GET",
-		url: "/desafio3/info/dados",
-	}).done(function(resp){
-		updateList(resp);
-	})
-	.fail(function() {
-    	alert( "error" );
-  	})
+	setInterval(function(){updateList()},5000);
+		
 })
