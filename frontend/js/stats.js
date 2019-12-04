b01e98c7cdfa9326f58647c2b8f7de5938f011d1
root <eric.lee.santos@gmail.com>
2019-12-04 12:37:10 -0300

    reformatando grafico de linha

--- frontend/js/stats.js
+++ frontend/js/stats.js
@@ -117,7 +117,7 @@ function lineChart(){
 			resp.forEach(function(item,index){
 				deltat[index]=(item.soma_por_dia)/3600;
 				let temp = new Date(item.data_maq);
-				let temp2 = temp.getDays() + "/" + temp.getMonths();
+				let temp2 = temp.getDate() + "/" + temp.getMonth();
 				datas[index]= temp.toDateString();
 			})
 			var ctx = $("#lineChart");
