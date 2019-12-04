a41fe598b19fb655456faad080accd81e28db63d
root <eric.lee.santos@gmail.com>
2019-12-04 17:40:28 -0300

    TENTANDO CONSERTAR TIMEZONE

--- frontend/js/stats.js
+++ frontend/js/stats.js
@@ -216,7 +216,7 @@ function barChart(){
 			labels: ['7h','8h','9h','10h','11h','12h','13h','14h','15h','16h','17h','18h','19h','20h','21h','22h'],
 			datasets: [{
 				label: 'Running',
-				borderCOlor:'rgba(85, 202, 206, 0.6)',
+				borderColor:'rgba(85, 202, 206, 0.6)',
 				backgroundColor: '#55CACE',
 				data: relative_sum
 			}]
