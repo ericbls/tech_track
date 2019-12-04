a8862010639e692f6f51a938d7a5cfeab0097db9
root <eric.lee.santos@gmail.com>
2019-12-04 15:57:31 -0300

    reformatando grafico de linha

--- backend/api.js
+++ backend/api.js
@@ -11,6 +11,6 @@ exports.build = function(server){
     .post('/techtrack/func/dados', functions.add_data)
     .get('/techtrack/func/dados/stats', functions.get_last_data_stats)
     .get('/techtrack/func/dados/linha', functions.get_dados_grafico_linha)
-    .get('/techtrack/func/dados/barras', functions.get_dados_grafico_barras)
+    .get('/techtrack/func/dados/barra', functions.get_dados_grafico_barras)
 
 }
